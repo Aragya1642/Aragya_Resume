7c74880ff7ed4e4ec9aa7fbf2d78bcf8ef47ec11
Aragya Goyal <agoyal1642@gmail.com>
2024-06-17 19:42:35 -0400

    fixed merger script

--- merger.py
+++ merger.py
@@ -3,6 +3,6 @@ from pypdf import PdfMerger
 merger = PdfMerger()
 
 merger.append('Aragya_Resume\main.pdf')
-merger.append('Aragya_Resume\Aragya_Resume_QR_Codes\main.pdf')
+merger.append('Aragya_Resume\Aragya_Resume_QR_Codes\qr_code.pdf')
 merger.write('Aragya_Final_Resume.pdf')
 merger.close()

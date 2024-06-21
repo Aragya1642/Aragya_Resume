d935ce56b1f791f9697212b2302ea69708070722
Aragya Goyal <agoyal1642@gmail.com>
2024-06-21 12:55:00 -0400

    Split to create Professional Expreience Section

--- merger.py
+++ merger.py
@@ -2,7 +2,7 @@ from pypdf import PdfMerger
 
 merger = PdfMerger()
 
-merger.append('Aragya_Resume\main.pdf')
-merger.append('Aragya_Resume\Aragya_Resume_QR_Codes\qr_code.pdf')
+merger.append('main.pdf')
+merger.append('Aragya_Resume_QR_Codes\qr_code.pdf')
 merger.write('Aragya_Final_Resume.pdf')
 merger.close()
